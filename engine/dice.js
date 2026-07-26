97fdeceb039da2a3f343b184a8df994b3afc1f80
MetinGames <metinbakir@outlook.com>
2026-07-26 22:55:28 +0300

    Use game as single dice move state

--- engine/dice.js
+++ engine/dice.js
@@ -3,37 +3,24 @@
 export class Dice {
     constructor() {
         this.values = [];
-        this.movesLeft = [];
     }
 
     roll() {
         const die1 = Math.floor(Math.random() * 6) + 1;
         const die2 = Math.floor(Math.random() * 6) + 1;
         this.values = [die1, die2];
-        this.movesLeft = die1 === die2
+
+        const moves = die1 === die2
             ? [die1, die1, die1, die1]
             : [die1, die2];
 
         return {
             values: [...this.values],
-            movesLeft: [...this.movesLeft]
+            moves
         };
-    }
-
-    useMove(moveValue) {
-        const index = this.movesLeft.indexOf(moveValue);
-        if (index === -1) return false;
-
-        this.movesLeft.splice(index, 1);
-        return true;
     }
 
     reset() {
         this.values = [];
-        this.movesLeft = [];
-    }
-
-    hasMoves() {
-        return this.movesLeft.length > 0;
     }
 }
