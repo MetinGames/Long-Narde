25e1bf7e0986fe4357843df9efe61fb166fc75a3
MetinGames <metinbakir@outlook.com>
2026-07-26 18:50:23 +0300

    Reset dice state cleanly

--- engine/dice.js
+++ engine/dice.js
@@ -2,41 +2,37 @@
 
 export class Dice {
     constructor() {
-        this.values = []; // Son atılan zarların değerleri
-        this.movesLeft = [];  // Bu zarlarla yapılabilecek kalan hamleler
+        this.values = [];
+        this.movesLeft = [];
     }
 
-    // 1-6 arasında rastgele iki zar üretir
     roll() {
         const die1 = Math.floor(Math.random() * 6) + 1;
         const die2 = Math.floor(Math.random() * 6) + 1;
-        
         this.values = [die1, die2];
-
-        // Narde Kuralı: Çift gelirse 4 defa oynanır, farklı gelirse 2 defa
-        if (die1 === die2) {
-            this.movesLeft = [die1, die1, die1, die1];
-        } else {
-            this.movesLeft = [die1, die2];
-        }
+        this.movesLeft = die1 === die2
+            ? [die1, die1, die1, die1]
+            : [die1, die2];
 
         return {
-            values: this.values,
-            movesLeft: [...this.movesLeft] // Kopyasını dönüyoruz
+            values: [...this.values],
+            movesLeft: [...this.movesLeft]
         };
     }
 
-    // Bir hamle yapıldığında o zarı listeden düşer
     useMove(moveValue) {
         const index = this.movesLeft.indexOf(moveValue);
-        if (index !== -1) {
-            this.movesLeft.splice(index, 1);
-            return true;
-        }
-        return false; // Geçersiz hamle değeri girilirse
+        if (index === -1) return false;
+
+        this.movesLeft.splice(index, 1);
+        return true;
     }
 
-    // Kalan hamle var mı kontrolü
+    reset() {
+        this.values = [];
+        this.movesLeft = [];
+    }
+
     hasMoves() {
         return this.movesLeft.length > 0;
     }
